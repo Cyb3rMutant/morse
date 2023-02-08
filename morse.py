4c495fa01649f8a4211c0c78524b8bd917501311
yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2023-02-08 17:39:42 +0000

    changed left and right to a dict with keys being . and -

--- morse.py
+++ morse.py
@@ -1,41 +1,27 @@
 class Binary_tree():
     def __init__(self, data : str = None) -> None:
-        self.__left : Binary_tree = None
         self.__data : str = data
-        self.__right : Binary_tree = None
+        self.__sides : dict = {'.' : None, '-' : None}
 
     def insert(self, data : str, encoding : str):
-        print(encoding)
-        if (encoding[0] == '.'):
-            if self.__left:
-                if not encoding:
-                    return 1 
-                return self.__left.insert(data, encoding[1:])
+        if not encoding:
+            return 1 
 
-            if len(encoding) != 1:
-                return 1 
+        if self.__sides[encoding[0]]:
+            return self.__sides[encoding[0]].insert(data, encoding[1:])
 
-            self.__left = Binary_tree(data)
-            return 0
+        if len(encoding) != 1:
+            return 1 
 
-        elif (encoding[0] == '-'):
-            if self.__right:
-                if not encoding:
-                    return 1 
-                return self.__right.insert(data, encoding[1:])
-
-            if len(encoding) != 1:
-                return 1
-
-            self.__right = Binary_tree(data)
-            return 0
+        self.__sides[encoding[0]] = Binary_tree(data)
+        return 0
 
     def print(self, indent : int = 0) -> None:
         print("    "*indent, self.__data)
 
-        if self.__left: self.__left.print(indent+1)
+        if self.__sides['.']: self.__sides['.'].print(indent+1)
 
-        if self.__right: self.__right.print(indent+1)
+        if self.__sides['-']: self.__sides['-'].print(indent+1)
 
 if __name__ == "__main__":
     x = Binary_tree("")
