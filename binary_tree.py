ce6c27aef9f15387736dc7c764bd3e4ace7d2ff4
yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2023-02-19 22:25:53 +0000

    added delete and find functions to the binary tree and made the morse class inherit from binary tree and only has 2 additional functions for encode and decode that call the find functions

--- binary_tree.py
+++ binary_tree.py
@@ -9,7 +9,16 @@ class Binary_tree():
     def __getitem__(self, key: str):
         return self.__sides[key]
 
-    def insert(self, data: str, encoding: str):
+    def __str__(self) -> str:
+        return str(self.__data)
+
+    def is_empty(self)->bool:
+        for node in self.__sides.values():
+            if node:
+                return False
+        return not self.__data
+
+    def insert(self, data: str, encoding: str) -> int:
         if not encoding:  # make sure encoding is not NULL
             return 1
 
@@ -23,14 +32,54 @@ class Binary_tree():
         self.__sides[encoding[0]] = Binary_tree(data)  # insert the new node
         return 0
 
-    def __str__(self) -> str:
-        return (self.__data)
+    def delete(self, data) -> None:
+        for s, node in self.__sides.items():
+            if node == data:
+                self.__sides[s] = None
+                break
+            elif not node:
+                continue
+            node.delete(data)
+
+    def find_path(self, elem: str, side: str = '') -> str:
+        if self == elem:
+            return side
+
+        for s, node in self.__sides.items():
+            if not node:
+                continue
+
+            if x := node.find_path(elem, s):
+                return side + x
+        return ''
+
+    def find_elem(self, path: str) -> str:
+        node = self
+
+        while len(path) > 1:
+            node = node[path[0]]
+
+            path = path[1:]
+
+        return str(node[path])
 
     def print(self, indent: int = 0) -> None:  # print the tree in preorder form
         print("\t"*indent, indent, self.__data)
 
-        if self['.']:
-            self['.'].print(indent+1)
+        for node in self.__sides.values():
+            if node:
+                node.print(indent+1)
 
-        if self['-']:
-            self['-'].print(indent+1)
+    def __del__(self) -> None:
+        print("item %s deleted", self)
+
+
+x = Binary_tree()
+x.insert('E', ".")
+x.insert('T', "-")
+x.insert('I', "..")
+x.insert('A', "...")
+x.print()
+x.delete("I")
+x.print()
+# print(x.find_elem(x.find_path("A")))
--- morse.py
+++ morse.py
@@ -1,107 +1,86 @@
 from binary_tree import Binary_tree
 
-class Morse():
-    def __init__(self) -> None:
-        self.__tree = Binary_tree("")
 
-        self.__tree.insert('E', ".")
-        self.__tree.insert('T', "-")
+class Morse(Binary_tree):
+    def __init__(self, data: str = None) -> None:
+        super().__init__(data)
 
-        self.__tree.insert('I', "..")
-        self.__tree.insert('A', ".-")
-        self.__tree.insert('N', "-.")
-        self.__tree.insert('M', "--")
+        self.insert('E', ".")
+        self.insert('T', "-")
 
-        self.__tree.insert('S', "...")
-        self.__tree.insert('U', "..-")
-        self.__tree.insert('R', ".-.")
-        self.__tree.insert('W', ".--")
-        self.__tree.insert('D', "-..")
-        self.__tree.insert('K', "-.-")
-        self.__tree.insert('G', "--.")
-        self.__tree.insert('O', "---")
+        self.insert('I', "..")
+        self.insert('A', ".-")
+        self.insert('N', "-.")
+        self.insert('M', "--")
 
-        self.__tree.insert('H', "....")
-        self.__tree.insert('V', "...-")
-        self.__tree.insert('F', "..-.")
-        self.__tree.insert(' ', "..--")
-        self.__tree.insert('L', ".-..")
-        self.__tree.insert(' ', ".-.-")
-        self.__tree.insert('P', ".--.")
-        self.__tree.insert('J', ".---")
-        self.__tree.insert('B', "-...")
-        self.__tree.insert('X', "-..-")
-        self.__tree.insert('C', "-.-.")
-        self.__tree.insert('Y', "-.--")
-        self.__tree.insert('Z', "--..")
-        self.__tree.insert('Q', "--.-")
-        self.__tree.insert(' ', "---.")
-        self.__tree.insert(' ', "----")
+        self.insert('S', "...")
+        self.insert('U', "..-")
+        self.insert('R', ".-.")
+        self.insert('W', ".--")
+        self.insert('D', "-..")
+        self.insert('K', "-.-")
+        self.insert('G', "--.")
+        self.insert('O', "---")
 
-        self.__tree.insert('5', ".....")
-        self.__tree.insert('4', "....-")
-        self.__tree.insert(' ', "...-.")
-        self.__tree.insert('3', "...--")
-        self.__tree.insert('¿', "..-.-")
-        self.__tree.insert('?', "..--.")
-        self.__tree.insert('2', "..---")
-        self.__tree.insert('&', ".-...")
-        self.__tree.insert(' ', ".-..-")
-        self.__tree.insert('+', ".-.-.")
-        self.__tree.insert('1', ".----")
-        self.__tree.insert('6', "-....")
-        self.__tree.insert('(', "-.--.")
-        self.__tree.insert('7', "--...")
-        self.__tree.insert('8', "---..")
-        self.__tree.insert('9', "----.")
-        self.__tree.insert('0', "-----")
+        self.insert('H', "....")
+        self.insert('V', "...-")
+        self.insert('F', "..-.")
+        self.insert(' ', "..--")
+        self.insert('L', ".-..")
+        self.insert(' ', ".-.-")
+        self.insert('P', ".--.")
+        self.insert('J', ".---")
+        self.insert('B', "-...")
+        self.insert('X', "-..-")
+        self.insert('C', "-.-.")
+        self.insert('Y', "-.--")
+        self.insert('Z', "--..")
+        self.insert('Q', "--.-")
+        self.insert(' ', "---.")
+        self.insert(' ', "----")
 
-        self.__tree.insert(' ', "...-..")
-        self.__tree.insert('_', "..--.-")
-        self.__tree.insert('"', ".-..-.")
-        self.__tree.insert("'", ".----.")
-        self.__tree.insert('-', "-....-")
-        self.__tree.insert(';', "-.-.-.")
-        self.__tree.insert('!', "-.-.--")
-        self.__tree.insert(')', "-.--.-")
-        self.__tree.insert('¡', "--...-")
+        self.insert('5', ".....")
+        self.insert('4', "....-")
+        self.insert(' ', "...-.")
+        self.insert('3', "...--")
+        self.insert('¿', "..-.-")
+        self.insert('?', "..--.")
+        self.insert('2', "..---")
+        self.insert('&', ".-...")
+        self.insert(' ', ".-..-")
+        self.insert('+', ".-.-.")
+        self.insert('1', ".----")
+        self.insert('6', "-....")
+        self.insert('(', "-.--.")
+        self.insert('7', "--...")
+        self.insert('8', "---..")
+        self.insert('9', "----.")
+        self.insert('0', "-----")
 
-        self.__tree.insert('$', "...-..-")
+        self.insert(' ', "...-..")
+        self.insert('_', "..--.-")
+        self.insert('"', ".-..-.")
+        self.insert("'", ".----.")
+        self.insert('-', "-....-")
+        self.insert(';', "-.-.-.")
+        self.insert('!', "-.-.--")
+        self.insert(')', "-.--.-")
+        self.insert('¡', "--...-")
 
-        # self.__tree.print()
+        self.insert('$', "...-..-")
 
-    def __get_morse_encoding(self, node, char, code) -> str:
-        if not node:
-            return ''
-        elif node == char:
-            return code
-
-        for side in ['.', '-']:
-            if x := self.__get_morse_encoding(node[side], char, side):
-                code += x
-                return code
+        self.print()
 
     def encode(self, message: str) -> str:
         encoded_message: str = ''
         for char in message:
-            encoded_message += self.__get_morse_encoding(
-                self.__tree, char, '') + ' '
+            encoded_message += self.find_path(char) + ' '
         return encoded_message[:-1]
-
-    def __get_decoded_char(self, code: str) -> str:
-        node = self.__tree
-
-        while len(code) > 1:
-            node = node[code[0]]
-
-            code = code[1:]
-
-        return str(node[code])
 
     def decode(self, message: str) -> str:
         decoded_message: str = ''
         for code in message.split(" "):
-            decoded_message += self.__get_decoded_char(code)
+            decoded_message += self.find_elem(code)
         return decoded_message
 
 
