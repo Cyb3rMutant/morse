04290c7ea9598149f5c799b41ec51e6e3e45cf97
yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2023-03-27 03:40:12 +0100

    echo server

---
+++ morse_server.py
@@ -0,0 +1,55 @@
+import asyncio
+import websockets
+import json
+from morse import morse
+
+
+async def send_message(websocket, message, client_id):
+    outwar_message = {
+        "type": "morse_evt",
+        "client_id": client_id,
+        "payload": message
+    }
+
+    await websocket.send(json.dumps(outwar_message))
+
+
+async def recv_message(websocket):
+    message = json.loads(await websocket.recv())
+    return message['payload']
+
+
+async def echo(websocket, message, client_id):
+    await send_message(websocket, message, client_id)
+
+    response = morse.decode_ham(await recv_message(websocket))
+
+    print("The Server Sent Back: ")
+    print(response)
+
+    return response
+
+
+async def read_message():
+    message = input("message: ")
+    return message
+
+
+async def main():
+    uri = "ws://localhost:10102"
+    async with websockets.connect(uri) as websocket:
+        message = json.loads(await websocket.recv())
+
+        if message["type"] != "join_evt":
+            print("Did not receive a correct join message")
+            return
+
+        client_id = message["client_id"]
+
+        user_message = await read_message()
+        while (await echo(websocket, morse.encode_ham("user", "echo", user_message), client_id) != "end"):
+            user_message = await read_message()
+
+if __name__ == "__main__":
+    print("Echo client")
+    asyncio.run(main())
